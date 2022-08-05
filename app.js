eb1c31df307827a057feceb2985b9e2d00cc74d9
Saaim-K <saaimkhan221@gmail.com>
2022-08-05 17:07:16 +0500

    Carousel

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let img = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'];
 function handler(event) {
-    let x = event.offsetX;
-    let clientWidth = event.target.clientWidth;
-    let clickPercent = Math.floor((x) * 100 / clientWidth);
-    let currentImage = document.getElementById('slide').src;
-    let currentUrl = currentImage.split('images/')[1];
-    let currentIndex = (img.indexOf(currentUrl));
-    let nextIndex = (img.indexOf(currentUrl) + 1);
+    let x = event.offsetX; //Where the mouse is clicked on the x axis
+    let clientWidth = event.target.clientWidth; //Screen width
+    let clickPercent = Math.floor((x) * 100 / clientWidth); //Percentage of the screen clicked
+    let currentImage = document.getElementById('slide').src; //Getting src of the current image
+    let currentUrl = currentImage.split('images/')[1]; //Splitting the src to get the image name
+    let currentIndex = (img.indexOf(currentUrl)); //Using current URL to get the index of the image from the array
+    let nextIndex = (img.indexOf(currentUrl) + 1); //Getting the index of the next image
     if (clickPercent > 25) {
-        document.getElementById('slide').src = `images/${img[nextIndex]}`;
+        document.getElementById('slide').src = `images/${img[nextIndex]}`;//Changing the src of the image using template literal
         if (nextIndex == img.length) {
             document.getElementById('slide').src = `images/${img[0]}`;
         }
@@ -33,7 +33,7 @@ function handler(event) {
 setInterval(() => {
     let currentImage = document.getElementById('slide').src;
     let currentUrl = currentImage.split('images/')[1];
-    let currentIndex = (img.indexOf(currentUrl));
+    let currentIndex = (img.indexOf(currentUrl)); //
     let nextIndex = (img.indexOf(currentUrl) + 1);
     document.getElementById('slide').src = `images/${img[nextIndex]}`;
     if (nextIndex == img.length) {
